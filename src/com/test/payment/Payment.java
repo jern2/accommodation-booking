48b92962f38da61eca0fdd0cc1666a502a7185f0
jimin <lemonjam0408@gmail.com>
2025-01-06 17:58:16 +0900

    create payment

--- src/com/test/payment/Payment.java
+++ src/com/test/payment/Payment.java
@@ -1,5 +1,19 @@
 package com.test.payment;
 
+import java.util.Scanner;
+
 public class Payment {
+	
+	private String paymentId;
+	private String userId;
+	private int amount;
+	private String cardnumber;
+	private String 
+	
+	
+		
+		
+	}
+	
 
 }
