d78c6bf6381cc7bb7e00643c78d40d084f5ff2c4
ParkJuseung <judang0322@gmail.com>
2025-01-06 17:56:38 +0900

    create booking class

--- src/com/test/booking/BookingService.java
+++ src/com/test/booking/BookingService.java
@@ -1,5 +1,37 @@
 package com.test.booking;
 
+import com.test.accommodation.Review;
+import com.test.user.User;
+
+
 public class BookingService {
 	
+	ReviewManager reviewManager = new ReviewManager();
+	
+	
+	//예약 생성 전체 처리
+	public Booking createBooking(String bookingId, User user, String accommodationId, String checkInDate, String checkOutDate, int GuestNum) {
+		return null;
+	}
+	
+	//유저의 예약 조회
+	public void getBookings(User user) {
+		
+	}
+	
+	//예약 변경
+	public void updateBooking(String bookingId, String newCheckIn, String newCheckOut, String newNumGuest) {
+		
+	}
+	
+	//예약 취소
+	public void cancelBooking(String bookinId, String password) {
+		
+	}
+	
+	//리뷰 작성
+	public void addReview(int bookingId, String content, int rating) {
+		Review review = new Review(rating, content, rating, content, rating);
+		
+	}
 }
